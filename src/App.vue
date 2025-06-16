b4bb7d1065afaeed97c0647b5bbd50a4efe36849
chenzheng.chen <chenzheng.chen@zhaopin.com.cn>
2025-06-16 17:11:12 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
-<script setup lang="ts">
-import largeFileUpload from "./components/largeFileUpload/index.vue";
 
-</script>
 
 <template>
   <div class="wrapper">
@@ -9,66 +6,11 @@ import largeFileUpload from "./components/largeFileUpload/index.vue";
   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup lang="ts">
+import largeFileUpload from "./components/largeFileUpload/index.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+</script>
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+<style scoped lang="scss">
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
